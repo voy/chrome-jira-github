2f2d5d45a5ede57d7bb2751663a6e03770d92b2c
Vojtech Jasny <vojtech.jasny@gooddata.com>
2014-06-16 15:15:55 +0200

    Poll for uri changes

--- linker.js
+++ linker.js
@@ -2,23 +2,49 @@ var template = 'https://jira.intgdc.com/browse/$0';
 
 var RE_JIRA_ISSUE = /[A-Z]{2,3}-[0-9]+/g;
 
+function issueIdToLink(issueId) {
+    var uri = template.replace('$0', issueId);
+
+    var node = document.createElement('a');
+    node.setAttribute('href', uri);
+    node.setAttribute('target', '_blank');
+    node.style.color = '#4183c4';
+    node.innerText = issueId;
+
+    return node.outerHTML;
+}
+
 function createLinks() {
+    // pull request detail
     var titles = document.querySelectorAll('.js-issue-title');
-    console.log(titles);
     [].slice.apply(titles).forEach(function(node) {
-        var newTitle = node.innerText.replace(RE_JIRA_ISSUE, function(str) {
-            var uri = template.replace('$0', str);
+        node.innerHTML = node.innerText.replace(RE_JIRA_ISSUE, issueIdToLink);
+    });
 
-            var node = document.createElement('a');
-            node.setAttribute('href', uri);
-            node.setAttribute('target', '_blank');
-            node.innerText = str;
+    // pull request list
+    titles = document.querySelectorAll('.pulls-list .js-navigation-open');
+    [].slice.apply(titles).forEach(function(node) {
+        var clone = node.cloneNode(true);
+        clone.innerHTML = '';
+        var startTag = clone.outerHTML.split('</')[0];
 
-            return node.outerHTML;
+        var str = node.innerText.replace(RE_JIRA_ISSUE, function(issueId) {
+            var link = issueIdToLink(issueId);
+            return '</a>' + link + startTag;
         });
-        node.innerHTML = newTitle;
+        node.outerHTML = startTag + str + '</a>';
     });
 }
 
-window.addEventListener('onpopstate', createLinks);
+// poll for location changes - anyone knows better solution let me know
+var url = window.location + '';
+setInterval(function() {
+    var newUrl = window.location + '';
+    if (url !== newUrl) {
+        url = newUrl;
+        createLinks();
+    }
+}, 2000);
+
+// initial run
 createLinks();
